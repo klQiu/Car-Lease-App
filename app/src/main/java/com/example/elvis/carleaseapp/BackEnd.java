44d845242a5474aa0ce98d5e5e2fd6a3d6deb4b7
Betty <apple@dhcp-128-189-253-63.ubcsecure.wireless.ubc.ca>
2017-10-12 17:59:26 -0700

    add User and Backend Class

---
+++ app/src/main/java/com/example/elvis/carleaseapp/BackEnd.java
@@ -0,0 +1,27 @@
+package com.example.elvis.carleaseapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by apple on 2017/10/12.
+ */
+
+public class BackEnd {
+
+    public ArrayList<Post> get20Post() {
+        return null;
+    }
+    public ArrayList<Post> getHisPost() {
+        return null;
+    }
+    public void addPost(Post post) {
+
+    }
+    public void addUser(User user) {
+
+    }
+    public Boolean checkLogin(String email, String entPassword) {
+        return false;
+    }
+
+}
