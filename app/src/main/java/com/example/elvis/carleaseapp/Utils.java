8833615cca2e44e0d29e0aa3a8b6f2e2593c6b96
Luxi Zhao <lucyzhao.zlx@gmail.com>
2017-10-17 14:00:43 -0700

    changed png to jpeg

--- app/src/main/java/com/example/elvis/carleaseapp/Utils.java
+++ app/src/main/java/com/example/elvis/carleaseapp/Utils.java
@@ -19,7 +19,7 @@ public class Utils {
     public static byte[] imgToByteArray(Bitmap b) {
         //Bitmap b = BitmapFactory.decodeResource(context.getResources(),R.drawable.test_img);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        b.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        b.compress(Bitmap.CompressFormat.JPEG, 100, stream);
         return stream.toByteArray();
     }
 
