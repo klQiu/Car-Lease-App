97983b759ac590dd8b31bae40b19f5e3e15de71f
Betty <apple@dhcp-128-189-249-123.ubcsecure.wireless.ubc.ca>
2017-11-30 14:53:34 -0800

    edit change psd

--- app/src/main/java/com/example/elvis/carleaseapp/ChangePsdActivity.java
+++ app/src/main/java/com/example/elvis/carleaseapp/ChangePsdActivity.java
@@ -1,33 +1,43 @@
 package com.example.elvis.carleaseapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class ChangePsdActivity extends AppCompatActivity {
+    PopupWindow popUpWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_psd);
+
+        //add popup window for warning msg
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View warnLayout = inflater.inflate(R.layout.warn, null);
+        popUpWindow = new PopupWindow(warnLayout, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,true);
     }
 
     public void subNewPsd(View view) {
         EditText oldPsdText = (EditText)findViewById(R.id.oldPsdText);
-        TextView msg = (TextView)findViewById(R.id.changePsdMsg);
         String oldPsd = oldPsdText.getText().toString();
         String correctPst = Current.getCurUser().getPassword();
+        EditText newPsd1Text = (EditText)findViewById(R.id.newPsdText);
+        EditText newPsd2Test = (EditText)findViewById(R.id.conNewPsdText);
+        String newPsd1 = newPsd1Text.getText().toString();
+        String newPsd2 = newPsd2Test.getText().toString();
+
         if(oldPsd.equals(correctPst)) {
-            EditText newPsd1Text = (EditText)findViewById(R.id.newPsdText);
-            EditText newPsd2Test = (EditText)findViewById(R.id.conNewPsdText);
-            String newPsd1 = newPsd1Text.getText().toString();
-            String newPsd2 = newPsd2Test.getText().toString();
             if(newPsd1.equals(newPsd2)) {
                 //change psd
-
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -48,13 +58,26 @@ public class ChangePsdActivity extends AppCompatActivity {
                 //print error msg
                 newPsd1Text.setText("");
                 newPsd2Test.setText("");
-                msg.setText("The passwords don't match. Please enter again");
+                //set warning msg
+                TextView warning = (TextView)popUpWindow.getContentView().findViewById(R.id.warnText);
+                warning.setText("The passwords don't match. Please enter again");
+                popUpWindow.showAtLocation(findViewById(R.id.changePsdLayout), Gravity.TOP, 0, 0);
+                popUpWindow.update(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             }
         }
         else{
             //print error msg
             oldPsdText.setText("");
-            msg.setText("The old password is not correct. Please enter again");
+            newPsd1Text.setText("");
+            newPsd2Test.setText("");
+            TextView warning = (TextView)popUpWindow.getContentView().findViewById(R.id.warnText);
+            warning.setText("The old password is not correct. Please enter again");
+            popUpWindow.showAtLocation(findViewById(R.id.changePsdLayout), Gravity.TOP, 0, 0);
+            popUpWindow.update(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         }
+    }
+
+    public void dismissWarn(View view) {
+        popUpWindow.dismiss();
     }
 }
